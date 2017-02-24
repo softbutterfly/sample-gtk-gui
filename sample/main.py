673e4e5783c4f7b7c190104f0ba369bfa57bc734
zodiacfireworks <martin.vuelta@gmail.com>
2017-02-23 22:14:58 -0500

    Ignite Project!!

---
+++ sample/main.py
@@ -0,0 +1,31 @@
+import gi
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
+
+import sqlite3
+import os
+
+working_directory = os.path.abspath(os.path.dirname(__file__))
+gui_file_path = os.path.join(working_directory, "sample.glade")
+
+
+class Handler:
+    def on_click_search_button(self, *args):
+        print("Alex me la pela")
+
+builder = Gtk.Builder()
+builder.add_from_file(gui_file_path)
+builder.connect_signals(Handler())
+
+def main():
+    global builder
+
+    win = builder.get_object("SampleWindow")
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
+
+
+if __name__ == '__main__':
+    main()
